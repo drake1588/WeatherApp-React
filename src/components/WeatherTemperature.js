9e31b3a17476c1df788b052b03a3b67a57e9c22e
Alfonso <alfo704@gmail.com>
2018-08-06 23:42:18 -0400

    Added weather-icons

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
 const WeatherTemperature = () => (
-    <div><span>25°</span></div>
+    <div>
+        <WeatherIcons name="cloud" size="2x"/>
+        <span>25°</span>
+    </div>
 );
 
 export default WeatherTemperature;
